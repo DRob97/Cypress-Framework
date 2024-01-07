f31231d15d82f0d1567bd56698d0948b369a42d6
DRob97 <dylanlrobertson@gmail.com>
2024-01-07 11:49:17 -0600

    added two more test files

---
+++ cypress/e2e/06-Selectors-VISIBLE-TEXT.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+// 
+describe('Selector | Visible Text', () => {
+    it('Validate Frontend Cards', () => {
+        cy.visit('https://techglobal-training.com/frontend')
+        // Locate the elements which contains Tables text
+        cy.contains('Tables')
+        // Locate the elements which contains Project text
+        cy.contains('Project')
+        // Locate the elements which contains Elements text
+        cy.contains('Elements')
+        // Locate the elements which contains Actions text
+        cy.contains('Actions')
+    })
+})
