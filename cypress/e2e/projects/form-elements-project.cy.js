3728783fa1e05503c7088ac04b21b9543b24ba54
DRob97 <dylanlrobertson@gmail.com>
2024-01-09 16:53:18 -0600

    did first 3 parts of form-elements-project

--- cypress/e2e/projects/form-elements-project.cy.js
+++ cypress/e2e/projects/form-elements-project.cy.js
@@ -1,2 +1,43 @@
 /// <reference types="cypress"/>
 
+describe('Form Elements Project', () => {
+    beforeEach(() => {
+        cy.visit('https://techglobal-training.com/frontend/project-1')
+    })
+
+    it('Test Case 01 - Validate the "Contact Us" information', () => {
+
+        cy.get('.is-size-3').should('have.text', 'Contact Us')
+        cy.get('#address').should('have.text', '2800 S River Rd Suite 310, Des Plaines, IL 60018')
+        cy.get('#email').should('have.text', 'info@techglobalschool.com')
+        cy.get('#phone-number').should('have.text', '(224) 580-2150')
+    })
+
+    it('Test Case 02 - Validate the "Full name" input box', () => {
+
+        cy.get('input[placeholder$="full name"]').should('be.visible')
+        cy.get('input[placeholder$="full name"]').should('have.attr', 'required')
+        cy.get('form > :nth-child(1) > label').should('have.text', 'Full name *')
+        cy.get('input[placeholder$="full name"]').should('have.attr', 'placeholder').and('eq', 'Enter your full name')
+    })
+
+    it('Test Case 03 - Validate the "Gender" radio button', () => {
+        cy.get('form > :nth-child(2) > div > :nth-child(1)').should('have.text', 'Gender *')
+        cy.get('form > :nth-child(2) > div > :nth-child(2) > input').should('have.attr', 'required')
+        // cy.get('.mr-1').should('be.oneOf', ["Male", "Female", "Prefer not to disclose"])
+        cy.get('form > :nth-child(2) > div > :nth-child(2)').should('have.text', 'Male')
+        cy.get('form > :nth-child(2) > div > :nth-child(3)').should('have.text', 'Female')
+        cy.get('form > :nth-child(2) > div > :nth-child(4)').should('have.text', 'Prefer not to disclose')
+
+        cy.get('.mr-1').should('be.enabled').and('not.be.checked')
+
+        cy.get('form > :nth-child(2) > div > :nth-child(2) > input').click()
+        cy.get('form > :nth-child(2) > div > :nth-child(3) > input').should('be.enabled').and('not.be.checked')
+        cy.get('form > :nth-child(2) > div > :nth-child(4) > input').should('be.enabled').and('not.be.checked')
+        // Put step 7 here
+        cy.get('form > :nth-child(2) > div > :nth-child(3) > input').click()
+        cy.get('form > :nth-child(2) > div > :nth-child(2) > input').should('be.enabled').and('not.be.checked')
+        cy.get('form > :nth-child(2) > div > :nth-child(4) > input').should('be.enabled').and('not.be.checked')
+    })
+    
+})
