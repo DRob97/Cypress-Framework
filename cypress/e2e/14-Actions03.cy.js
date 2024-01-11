dd4764b7677093b2041d5308f1acd6e66a57ae2d
DRob97 <dylanlrobertson@gmail.com>
2024-01-10 21:27:28 -0600

    added a new test file and updated another

--- cypress/e2e/14-Actions03.cy.js
+++ cypress/e2e/14-Actions03.cy.js
@@ -40,4 +40,75 @@ describe('Actions', () => {
         .clear()
         .should('have.value', '')
     })
+
+    it('Actions | Check & Uncheck', () => {
+        cy.get('#apple_check').should('be.visible').and('have.text', 'Apple')
+        cy.get('#checkbox_1').should('be.visible').and('be.enabled')
+        .and('not.be.checked').check()
+        .should('be.checked').uncheck()
+        .should('not.be.checked')
+
+        cy.get('#tesla_check').should('be.visible').and('have.text', 'Tesla')
+        cy.get('#checkbox_3')
+        .should('be.visible')
+        .and('be.enabled')
+        .and('not.be.checked')
+        .check()
+        .should('be.checked')
+        .uncheck()
+        .should('not.be.checked')
+    })
+
+    it('Actions | Check Radio Buttons', () => {
+        cy.get('#js_radio').should('be.visible').and('have.text', 'JavaScript')
+
+        cy.get('#radio_1_option_2')
+        .should('be.visible')
+        .and('not.be.checked')
+        .check()
+        .should('be.checked')
+        
+        cy.get('#c_radio').should('be.visible').and('have.text', 'C#')
+
+        cy.get('#radio_1_option_3')
+        .should('be.visible')
+        .and('not.be.checked')
+        .check()
+        .should('be.checked')
+    })
+
+    it('Actions | Select', () => {
+        cy.get('#company_dropdown1').as('dropdown')
+        
+        cy.get('@dropdown').should('be.visible')
+        cy.get('#company_dropdown1 option:selected').should('have.text', 'Select a company...')
+
+        // 1. selecting by visible text
+        cy.get('#company_dropdown1').select('Microsoft')
+        cy.get('#company_dropdown1 option:selected').should('have.text', 'Microsoft')
+
+        // 2. selecting by value
+        cy.get('#company_dropdown1').select('Apple')
+        cy.get('#company_dropdown1 option:selected').should('have.text', 'Apple')
+        
+        // 3. selecting by index
+        cy.get('#company_dropdown1').select(3)
+        cy.get('#company_dropdown1 option:selected').should('have.text', 'Tesla')
+    })
+
+    it.only('Actions | Type Dates', () => {
+        cy.get('#date_input1').should('be.visible')
+        .should('have.attr', 'placeholder', 'mm/dd/yyyy')
+        .should('have.value', '')
+        .type('10/10/2000{enter}')
+        .should('have.value', '10/10/2000')
+
+        cy.get('#date_input2').should('be.visible')
+        .should('not.have.value', '')
+        .clear()
+        .type('10/10/2000{enter}')
+        .should('have.value', '10/10/2000')
+        .clear()
+        .should('have.value', '')
+    })
 })
