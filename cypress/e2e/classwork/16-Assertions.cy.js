5202f486c1594d7951a39899aaec84d113e75fdd
DRob97 <dylanlrobertson@gmail.com>
2024-01-14 13:58:48 -0600

    updated test 16 once more, removed a commented out block from cypress.config to see if that would help with the not running tests when I save problem

--- cypress/e2e/classwork/16-Assertions.cy.js
+++ cypress/e2e/classwork/16-Assertions.cy.js
@@ -228,9 +228,39 @@ describe("Assertions", () => {
      * 5. Validate text input 1 and text input 2 is is not required
      * 6. Enter your name and last name
      */
-    const names = ['Dylan', 'Robertson']
-    cy.get('[data-identifier="Text Inputs"] input').each((el, index) => {
-      cy.wrap(el).type(names[index]).should('be.enabled').and('not.have.attr', 'required')
+
+    const names = ['Tech', 'Global']
+
+    cy.get('[data-identifier="Text Inputs"] input').each(($el, index) => {
+        cy.wrap($el).type(names[index]).should('be.enabled').and('not.have.attr', 'required')
     })
+
+    /**
+         * 1. Go to https://techglobal-training.com/frontend
+         * 2. Navigate to 'Html Elements' card
+         * 3. From the "Date Inputs" section
+         * 4. Validate date input 1 and date input 2 is enabled
+         * 5. Validate date input 1 and date input 2 is is not required
+         * 6. Enter dates for both date input 1 and date input 2
+         * 7. Validate value is changed to given dates.
+         */
+    const dates = ['01/02/2003', '04/05/2006']
+    cy.get('[data-identifier="Date Inputs"] input').each((el, index) => {
+      cy.wrap(el).clear().type(`${dates[index]} {enter}`).should('be.enabled')
+      .and('have.value', dates[index]).and('not.have.attr', 'required')
     })
+
+    /**
+         * 1. Go to https://techglobal-training.com/frontend
+         * 2. Navigate to 'Html Elements' card
+         * 3. From the "Dropdowns" section
+         * 4. Validate dropdown 1 and dropdown 2 is enabled
+         * 6. Enter Microsoft for dropdown 1 and Apple for dropdown 2
+         * 7. Validate options are selected
+         */
+    const data = ['Microsoft', 'Apple']
+    cy.get('select[id^="company_dropdown"]').each((el, index) => {
+      cy.wrap(el).select(data[index]).should('be.enabled').and('have.value', data[index])
+    })
+  })
 });
