7bcf71625b51fd55facb9b8e684379b62dd239b3
DRob97 <dylanlrobertson@gmail.com>
2024-01-24 21:19:51 -0600

    added a new test file, a new POM file, and a new json file

---
+++ cypress/pages/TablesPage.js
@@ -0,0 +1,12 @@
+import BasePage from "./BasePage"
+
+class Tables extends BasePage{
+    // Locators
+        getTableHeaders(){
+            return cy.get('tr > .header')
+        }
+        
+    // Methods
+}
+
+export default Tables
